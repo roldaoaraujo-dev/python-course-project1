18e8034182b57c154f91452cfdc9828327da0dc3
Roldao Araujo <roldao.negociodigital@gmail.com>
2021-03-27 16:47:29 +0000

    [#Issue1] Create function movie_add and call it from the menu

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 # This list will store the user movies
 movies = []
 
-# This will be the structure of each movie
-movie = {
-    "title": "",
-    "director": "",
-    "release_year": 0
-}
 # Menu implementation
 MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
 
+
+def movie_add():
+    # Getting input from the user
+    title = input("Type the movie title: ")
+    director = input("Type the movie director: ")
+    release_year = input("Type the movie release year: ")
+
+    movie = {
+        "title": title,
+        "director": director,
+        "release_year": release_year
+    }
+    movies.append(movie)
+
+
 selection = input(MENU_PROMPT)
+
 while selection != 'q':
     if selection == 'a':
-        pass
+        movie_add()
+        print("The movie was added successfully")
     elif selection == 'l':
         pass
     elif selection == 'f':
